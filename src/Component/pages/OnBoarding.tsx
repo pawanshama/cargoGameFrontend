a5794458e234f3e688fdc411f253fa67ccc3a143
Adam Noe <abderrahmaneadam@rocketmail.com>
2025-06-22 14:44:15 +0400

    Fixing

--- src/Component/pages/OnBoarding.tsx
+++ src/Component/pages/OnBoarding.tsx
@@ -134,7 +134,7 @@ const OnBoarding = () => {
 
       // Appel API avec les données Telegram et l'inviteCode
       try {
-        const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/auth/create-or-find`, {
+        const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/auth/create-or-find`, {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
